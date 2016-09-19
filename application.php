921c0c8e5cff643a07a0f43f26e0c1ac68774236
Jon Klixbüll Langeland <langeland@users.noreply.github.com>
2016-09-19 08:19:59 +0200

    [FEATURE] Add root login parameter (#3)

--- application.php
+++ application.php
@@ -1,7 +1,6 @@
 #!/usr/bin/env php
 <?php
 
-
 if (file_exists(__DIR__ . '/Libraries/autoload.php')) {
 	require __DIR__ . '/Libraries/autoload.php';
 } else {
@@ -14,25 +13,38 @@ define('ROOT_DIRECTORY', __DIR__);
 
 
 if(is_file(BASE_DIRECTORY . '/docker-compose.yaml')) {
-	$value = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(BASE_DIRECTORY . '/docker-compose.yaml'));
+	$fileExtension = 'yaml';
 } elseif(is_file(BASE_DIRECTORY . '/docker-compose.yml')) {
-	$value = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(BASE_DIRECTORY . '/docker-compose.yml'));
+	$fileExtension = 'yml';
 } else {
 	die('No docker-compose file found.');
 }
 
+$file = BASE_DIRECTORY . '/docker-compose.' . $fileExtension;
+$value = \Symfony\Component\Yaml\Yaml::parse(file_get_contents($file));
+
+if(!is_array($value['mysql'])){
+	die('Cannot find a mysql container.');
+}
+
 $portmapping = explode(':', $value['mysql']['ports'][0]);
-$markers = array(
-	'###DATABASE###' => $value['mysql']['environment']['MYSQL_DATABASE'],
-	'###USER###' => $value['mysql']['environment']['MYSQL_USER'],
-	'###HOST###' => '127.0.0.1',
-	'###PASSWORD###' => $value['mysql']['environment']['MYSQL_PASSWORD'],
-	'###PORT###' => $portmapping[0]
-	);
+
+$markers = array();
+if(in_array('-r', $argv)){
+	$markers['###USER###'] = 'root';
+	$markers['###PASSWORD###'] = $value['mysql']['environment']['MYSQL_ROOT_PASSWORD'];
+} else {
+	$markers['###USER###'] = $value['mysql']['environment']['MYSQL_USER'];
+	$markers['###PASSWORD###'] = $value['mysql']['environment']['MYSQL_PASSWORD'];
+}
+
+$markers['###DATABASE###'] = $value['mysql']['environment']['MYSQL_DATABASE'];
+$markers['###HOST###'] = '127.0.0.1';
+$markers['###PORT###'] = $portmapping[0];
+
 
 $spfTemplate = file_get_contents(ROOT_DIRECTORY . '/spf-template.spf');
 $spfContent = str_replace(array_keys($markers), array_values($markers), $spfTemplate);
-
 $tmpfname = tempnam("/tmp", "DOCKER_SEQUALPRO");
 
 file_put_contents($tmpfname . '.spf', $spfContent);
